7b1568cbae1147769bad98b605d00abb9dc26751
Miguel Angel Ivars Mas <yowi82@gmail.com>
2023-06-06 18:01:18 +0200

    Add affiliate links and redirect

---
+++ get/index.js
@@ -0,0 +1,25 @@
+let url = window.location.href;
+let segments = url.split("/");
+let destiny = segments[4];
+console.log(destiny);
+switch (destiny) {
+    case "notion": 
+        window.location = "https://affiliate.notion.so/obteo";
+        break;
+        
+    case "streak": 
+        window.location = "https://get.streak.com/obteo";
+        break;
+
+    case "process": 
+        window.location = "https://get.process.st/obteo";
+        break;
+    
+    case "landbot": 
+        window.location = "https://get.landbotlab.com/obteo";
+        break;
+        
+    default:
+        window.location = "https://obteo.com";
+        break;
+}
